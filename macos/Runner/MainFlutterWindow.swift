b66cd9a78f03d195d07db3e43a75557402b130a6
OtisFietDev <otis@fiet.net>
2025-04-17 04:44:54 +0900

    chore : set minimum window size

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,17 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    override func awakeFromNib() {
+      let flutterViewController = FlutterViewController()
+      let windowFrame = self.frame
+      self.contentViewController = flutterViewController
+      self.setFrame(windowFrame, display: true)
+      
+      // 윈도우 최소 사이즈 설정
+      self.minSize = NSSize(width: 800, height: 600) // 원하는 최소 크기로 조정
+      
+      RegisterGeneratedPlugins(registry: flutterViewController)
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
-
-    super.awakeFromNib()
-  }
+      super.awakeFromNib()
+    }
 }
